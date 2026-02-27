52b4cb6c4376a40e32a7fd459ce26a736fde5790
Nwankpele Damilola <nwankpele@gmail.com>
2026-02-27 00:41:23 +0000

    netlify toml

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,8 +1,12 @@
-import app from '../../app.ts';
+import express, { Router } from "express";
 import serverless from "serverless-http";
+import app from "../../app.ts";
 
+const api = express();
 
+const router = Router();
+router.use("/api", app);
 
-export const handler = serverless(app);
+api.use("/.netlify/functions/api", router);
 
-
+export const handler = serverless(api);
